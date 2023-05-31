227cc8ee7bd56db0cac945f0ebba2bb252f0000b
tmyhrn <ht.tomoya0605@gmail.com>
2023-05-31 09:55:09 +0000

    [finish]task5-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'relationships/followings'
   get 'relationships/followers'
-  get "search" => "searches#search"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
@@ -17,5 +15,6 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  get '/search', to: 'searches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
