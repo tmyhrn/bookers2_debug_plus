5ad2412b80977adfa3b7d154f113bc2357c7da79
tmyhrn <ht.tomoya0605@gmail.com>
2023-06-04 08:36:59 +0000

    [finish]task8d

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,16 @@ class BooksController < ApplicationController
       sort_by {|x|
         x.favorited_users.includes(:favorites).where(created_at: from...to).size
       }.reverse
+      
+      if params[:latest]
+        @books = Book.latest
+      elsif params[:old]
+        @books = Book.old
+      elsif params[:star_count]
+        @books = Book.star_count
+      else
+        @books = Book.all
+      end
     @book = Book.new
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
-    has_many :read_counts, dependent: :destroy
+  has_many :read_counts, dependent: :destroy
   
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
@@ -19,6 +19,10 @@ class Book < ApplicationRecord
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } 
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
+  
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
   
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
