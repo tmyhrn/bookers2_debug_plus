d94ec83d1446d3bf786be8e75afdd66f1b1f79a5
tmyhrn <ht.tomoya0605@gmail.com>
2023-06-04 07:17:28 +0000

    [finish]task9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,9 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @book_comment = BookComment.new
+    unless ReadCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.read_counts.create(book_id: @book.id)
+    end
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
+    has_many :read_counts, dependent: :destroy
   
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
