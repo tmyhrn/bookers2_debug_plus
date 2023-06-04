3691efed569d6bb6884455eaef83b84218cfda85
tmyhrn <ht.tomoya0605@gmail.com>
2023-06-04 03:15:42 +0000

    [finish]task8a

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,9 @@ Rails.application.routes.draw do
     get "search", to: "users#search"
   end
   get '/search', to: 'searches#search'
+  
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
