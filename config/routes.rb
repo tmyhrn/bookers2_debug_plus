1507b675815feefed37f6b4f85d38078fe3ce078
tmyhrn <ht.tomoya0605@gmail.com>
2023-06-08 06:02:59 +0000

    [finish]task8c

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,9 @@ Rails.application.routes.draw do
   
   get 'tagsearches/search', to: 'tagsearches#search'
   
-  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
